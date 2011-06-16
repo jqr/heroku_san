490034d772d5e80a14cc6dfe62d16a37f1ac6e0f
Jonathan Vaught <jonathan.vaught@gmail.com>
2011-06-16 16:15:58 -0400

    Revert "Test"
    This reverts commit 93fd6469749d6e8758559e05035aa0cc2bb4f83d.

--- lib/heroku_san/tasks.rb
+++ lib/heroku_san/tasks.rb
@@ -293,12 +293,6 @@ task :logs do
   end
 end
 
-task :show_run_or_rake do
-  each_heroku_app do |name, app, repo|
-    puts "#{app} uses '#{run_or_rake(app)}'"
-  end
-end
-
 namespace :db do
   desc 'Pull the Heroku database'
   task :pull do
