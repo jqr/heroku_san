f113bd0cfa1077f03e66ed94c75098af7e7d3fb1
Kenneth Mayer <kmayer@bitwrangler.com>
2012-03-03 20:36:59 -0800

    Documentation update. Closes #72

--- lib/heroku_san/version.rb
+++ lib/heroku_san/version.rb
@@ -1,3 +1,3 @@
 module HerokuSan
-  VERSION = "2.0.rc.1"
+  VERSION = "2.1.0"
 end
--- lib/tasks.rb
+++ lib/tasks.rb
@@ -5,17 +5,18 @@ include Git
 
 @heroku_san.all.each do |stage|
   desc "Select #{stage} Heroku app for later commands"
-  task stage do
+  task "heroku:stage:#{stage}" do
     @heroku_san << stage
   end
-end
-
-desc 'Select all Heroku apps for later command'
-task :all do
-  @heroku_san << @heroku_san.all
+  task stage => "heroku:stage:#{stage}"
 end
 
 namespace :heroku do
+  desc 'Select all Heroku apps for later command'
+  task 'stage:all' do
+    @heroku_san << @heroku_san.all
+  end
+  
   desc "Creates the Heroku app"
   task :create do
     each_heroku_app do |stage|
@@ -23,7 +24,7 @@ namespace :heroku do
     end
   end
 
-  desc "Generate the Heroku gems manifest from gem dependencies"
+  #desc "Generate the Heroku gems manifest from gem dependencies"
   task :gems => 'gems:base' do
     raise HerokuSan::Deprecated
   end
@@ -35,7 +36,7 @@ namespace :heroku do
     end
   end
 
-  desc 'Adds a collaborator'
+  desc 'Adds a collaborator (asks for email)'
   task :share do
     print "Email address of collaborator to add: "
     $stdout.flush
@@ -45,7 +46,7 @@ namespace :heroku do
     end
   end
 
-  desc 'Removes a collaborator'
+  desc 'Removes a collaborator (asks for email)'
   task :unshare do
     print "Email address of collaborator to remove: "
     $stdout.flush
@@ -78,18 +79,6 @@ namespace :heroku do
     end
   end
 
-  desc 'Add proper RACK_ENV to each application'
-  task :rack_env => :all do
-    each_heroku_app do |stage|
-      command = "heroku config --app #{stage.app}"
-      puts command
-      config = Hash[`#{command}`.scan(/^(.+?)\s*=>\s*(.+)$/)]
-      if config['RACK_ENV'] != stage.name
-        stage.push_config RACK_ENV: stage.name
-      end
-    end
-  end
-
   desc 'Add config:vars to each application.'
   task :config do
     each_heroku_app do |stage|
@@ -113,6 +102,18 @@ namespace :heroku do
   end
 
   namespace :config do
+    desc 'Add proper RACK_ENV to each application'
+    task :rack_env => :all do
+      each_heroku_app do |stage|
+        command = "heroku config --app #{stage.app}"
+        puts command
+        config = Hash[`#{command}`.scan(/^(.+?)\s*=>\s*(.+)$/)]
+        if config['RACK_ENV'] != stage.name
+          stage.push_config RACK_ENV: stage.name
+        end
+      end
+    end
+
     desc "Lists config variables as set on Heroku"
     task :list do
       each_heroku_app do |stage|
@@ -176,99 +177,113 @@ namespace :heroku do
       stage.maintenance :off
     end
   end
-end
 
-desc "Pushes the given commit, migrates and restarts (default: HEAD)"
-task :deploy, [:commit] => [:before_deploy] do |t, args|
-  each_heroku_app do |stage|
-    stage.deploy(args[:commit])
-    stage.migrate
-  end
-  Rake::Task[:after_deploy].execute
-end
-
-namespace :deploy do
-  desc "Force-pushes the given commit, migrates and restarts (default: HEAD)"
-  task :force, [:commit] => [:before_deploy] do |t, args|
+  desc "Pushes the given commit, migrates and restarts (default: HEAD)"
+  task :deploy, [:commit] => [:before_deploy] do |t, args|
     each_heroku_app do |stage|
-      stage.deploy(args[:commit], :force)
+      stage.deploy(args[:commit])
       stage.migrate
     end
     Rake::Task[:after_deploy].execute
   end
-end
 
-task :force_deploy do
-  raise Deprecated
-end
+  namespace :deploy do
+    desc "Force-pushes the given commit, migrates and restarts (default: HEAD)"
+    task :force, [:commit] => [:before_deploy] do |t, args|
+      each_heroku_app do |stage|
+        stage.deploy(args[:commit], :force)
+        stage.migrate
+      end
+      Rake::Task[:after_deploy].execute
+    end
 
-desc "Callback before deploys"
-task :before_deploy do
-end
+    desc "Callback before deploys"
+    task :before do
+    end
 
-desc "Callback after deploys"
-task :after_deploy do
-end
-
-desc "Captures a bundle on Heroku"
-task :capture do
-  raise Deprecated
-end
-
-desc "Opens a remote console"
-task :console do
-  each_heroku_app do |stage|
-    stage.run 'console'
+    desc "Callback after deploys"
+    task :after do
   end
-end
 
-desc "Restarts remote servers"
-task :restart do
-  each_heroku_app do |stage|
-    stage.restart
   end
-end
 
-desc "Migrates and restarts remote servers"
-task :migrate do
-  each_heroku_app do |stage|
-    stage.migrate
+  task :force_deploy do
+    raise HerokuSan::Deprecated
   end
-end
 
-namespace :logs do
+  #desc "Captures a bundle on Heroku"
+  task :capture do
+    raise HerokuSan::Deprecated
+  end
+
+  desc "Opens a remote console"
+  task :console do
+    each_heroku_app do |stage|
+      stage.run 'console'
+    end
+  end
+
+  desc "Restarts remote servers"
+  task :restart do
+    each_heroku_app do |stage|
+      stage.restart
+    end
+  end
+
+  namespace :logs do
+    task :default do
+      each_heroku_app do |stage|
+        stage.logs
+      end
+    end
+
+    desc "Tail the Heroku logs (requires logging:expanded)"
+    task :tail do
+      each_heroku_app do |stage|
+        stage.logs(:tail)
+      end
+    end
+  end
+
   desc "Shows the Heroku logs"
-  task :default do
-    each_heroku_app do |stage|
-      stage.logs
+  task :logs => 'logs:default'
+
+  namespace :db do
+    desc "Migrates and restarts remote servers"
+    task :migrate do
+      each_heroku_app do |stage|
+        stage.migrate
+      end
     end
-  end
 
-  desc "Tail the Heroku logs (requires logging:expanded)"
-  task :tail do
-    each_heroku_app do |stage|
-      stage.logs(:tail)
-    end
-  end
-end
-
-task :logs => 'logs:default'
-
-namespace :db do
-  task :pull do
-    each_heroku_app do |stage|
-      sh "heroku pgdumps:capture --app #{stage.app}"
-      dump = `heroku pgdumps --app #{stage.app}`.split("\n").last.split(" ").first
-      sh "mkdir -p #{Rails.root}/db/dumps"
-      file = "#{Rails.root}/db/dumps/#{dump}.sql.gz"
-      url = `heroku pgdumps:url --app #{stage.app} #{dump}`.chomp
-      sh "wget", url, "-O", file
-      sh "rake db:drop db:create"
-      sh "gunzip -c #{file} | #{Rails.root}/script/dbconsole"
-      sh "rake jobs:clear"
+    desc "Pull database from stage to local dev database"
+    task :pull do
+      each_heroku_app do |stage|
+        sh "heroku pgdumps:capture --app #{stage.app}"
+        dump = `heroku pgdumps --app #{stage.app}`.split("\n").last.split(" ").first
+        sh "mkdir -p #{Rails.root}/db/dumps"
+        file = "#{Rails.root}/db/dumps/#{dump}.sql.gz"
+        url = `heroku pgdumps:url --app #{stage.app} #{dump}`.chomp
+        sh "wget", url, "-O", file
+        sh "rake db:drop db:create"
+        sh "gunzip -c #{file} | #{Rails.root}/script/dbconsole"
+        sh "rake jobs:clear"
+      end
     end
   end
 end
+
+task :all => 'heroku:stage:all'
+task :deploy => 'heroku:deploy'
+task 'deploy:force' => 'heroku:deploy:force'
+task :before_deploy => 'heroku:deploy:before'
+task :after_deploy => 'heroku:deploy:after'
+task :console => 'heroku:console'
+task :restart => 'heroku:restart'
+task :migrate => 'heroku:db:migrate'
+task :logs => 'heroku:logs:default'
+task 'logs:tail' => 'heroku:logs:tail'
+task 'heroku:rack_env' => 'heroku:config:rack_env'
 
 def each_heroku_app(&block)
   @heroku_san.each_app(&block)
