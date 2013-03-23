4e16b8902ad3598fb6dcd9b07550c8245788bf18
Ken Mayer <pair+ken@pivotallabs.com>
2013-03-23 14:38:09 -0700

    Refactor project_spec
    - Replace fixture files with configuration hashes

--- spec/heroku_san/project_spec.rb
+++ spec/heroku_san/project_spec.rb
@@ -2,26 +2,30 @@ require 'spec_helper'
 require 'tmpdir'
 
 describe HerokuSan::Project do
-  let(:heroku_config_file) { File.join(SPEC_ROOT, "fixtures", "example.yml") }
-  let(:heroku_san) { HerokuSan::Project.new(heroku_config_file) }
+  let(:configuration) { {'production' => {}, 'staging' => {}, 'demo' => {}} }
+  let(:heroku_san) { HerokuSan::Project.new('') }
   subject { heroku_san }
+  before(:each) do
+    heroku_san.configuration = configuration
+  end
 
   describe ".new" do
     its(:all) { should =~ %w[production staging demo] }
     specify "with a missing config file has no stages" do
-      heroku_san = HerokuSan::Project.new("/u/should/never/get/here")
+      heroku_san.configuration = {}
       heroku_san.all.should == []
     end
-  
+
     specify "with a deploy option configures each stage with the strategy" do
-      heroku_san = HerokuSan::Project.new(heroku_config_file, :deploy => HerokuSan::Deploy::Base)
+      heroku_san.options[:deploy] = HerokuSan::Deploy::Base
+      heroku_san.configuration = configuration # yuck
       heroku_san << heroku_san.all
       heroku_san.each_app do |stage|
         stage.instance_variable_get('@options')['deploy'].should == HerokuSan::Deploy::Base
       end
     end
   end
-  
+
   describe "#apps constructs the deploy list" do
     it "appends known shorthands to apps" do
       heroku_san.apps.should == []
@@ -32,12 +36,12 @@ describe HerokuSan::Project do
       heroku_san << 'unknown'
       heroku_san.apps.should == %w[production staging]
     end
-  
+
     it "appends .all (or any array)" do
       heroku_san << heroku_san.all
       heroku_san.apps.should == heroku_san.all
     end
-  
+
     describe "extra (default) behaviors" do
       specify "on a git branch that matches an app name" do
         heroku_san.should_receive(:git_active_branch) { "staging" }
@@ -46,15 +50,15 @@ describe HerokuSan::Project do
           heroku_san.apps.should == %w[staging]
         }.to change{heroku_san.instance_variable_get('@apps')}.from([]).to(%w[staging])
       end
-    
+
       specify "on a git branch that doesn't matches an app name" do
         heroku_san.should_receive(:git_active_branch) { "master" }
         heroku_san.apps.should == %w[]
       end
-    
-      context "with only a single configured app" do        
-        let(:heroku_san) { HerokuSan::Project.new(File.join(SPEC_ROOT, "fixtures", "single_app.yml")) }
+
+      context "with only a single configured app" do
         it "returns the app" do
+          heroku_san.configuration = {'production' => {}}
           $stdout.should_receive(:puts).with('Defaulting to "production" since only one app is defined')
           expect {
             heroku_san.apps.should == %w[production]
@@ -63,12 +67,12 @@ describe HerokuSan::Project do
       end
     end
   end
-  
-  describe "#each_app" do    
+
+  describe "#each_app" do
     it "raises an error is no apps were specified" do
       expect { heroku_san.each_app do true end }.to raise_error HerokuSan::NoApps
     end
-    
+
     it "yields to a block with args" do
       heroku_san << 'production'
       block = double('block')
@@ -78,7 +82,7 @@ describe HerokuSan::Project do
       end
     end
   end
-  
+
   describe "#[]" do
     it "returns a config section" do
       heroku_san.all.each do |app|
