40860db7cfed603affda6db5ac2a52789b3c1e05
Ken Mayer <pair+ken@pivotallabs.com>
2013-03-23 13:45:55 -0700

    Backfill unit tests for the current format

--- spec/heroku_san/parser_spec.rb
+++ spec/heroku_san/parser_spec.rb
@@ -5,6 +5,36 @@ describe HerokuSan::Parser do
   Parseable = Struct.new(:config_file, :configuration)
 
   describe "#parse" do
+    context 'using the new format' do
+      let(:parseable) { Parseable.new(File.join(SPEC_ROOT, "fixtures", "example.yml")) }
+      it "returns a list of apps" do
+        parser.parse(parseable)
+        parseable.configuration.keys.should =~ %w[production staging demo]
+        parseable.configuration['production'].should == {
+            'app' => 'awesomeapp',
+            'tag' => 'production/*',
+            'config' => {
+                'BUNDLE_WITHOUT' => 'development:test',
+                'GOOGLE_ANALYTICS' => 'UA-12345678-1'
+            }
+        }
+        parseable.configuration['staging'].should == {
+            'app' => 'awesomeapp-staging',
+            'stack' => 'bamboo-ree-1.8.7',
+            'config' => {
+                'BUNDLE_WITHOUT' => 'development:test'
+            }
+        }
+        parseable.configuration['demo'].should == {
+            'app' => 'awesomeapp-demo',
+            'stack' => 'cedar',
+            'config' => {
+                'BUNDLE_WITHOUT' => 'development:test'
+            }
+        }
+      end
+    end
+
     context "using the heroku_san format" do
       let(:parseable) { Parseable.new(File.join(SPEC_ROOT, "fixtures", "old_format.yml")) }
 
