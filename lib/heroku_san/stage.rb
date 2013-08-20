d044febb9c69f19403628f3d9a419f8483c14427
Liron Yahdav <liron@handl.it>
2013-08-20 14:13:23 -0700

    Improve check for pending migrations: Run psql command like heroku toolbelt does. Get local migrations from source code rather than local database state.

--- lib/heroku_san/stage.rb
+++ lib/heroku_san/stage.rb
@@ -137,19 +137,36 @@ module HerokuSan
       return true unless defined?(::Rails)
       database_url = long_config['DATABASE_URL']
       return true unless database_url =~ /\Apostgres:\/\//
-      remote_migrations = get_remote_migrations(database_url)
-      Rake::Task[:environment].invoke
-      ActiveRecord::Base.establish_connection
-      local_migrations = ActiveRecord::Migrator.new(:up, ActiveRecord::Migrator.migrations_paths).migrations
-      pending_migrations = local_migrations.reject { |m| remote_migrations.include?(m.version.to_s) }
-      @has_pending_migrations = pending_migrations.present?
+      @has_pending_migrations = (local_migrations - remote_migrations(database_url)).present?
     end
 
   private
 
     # returns an array of the schema_migrations run on the server
-    def get_remote_migrations(database_url)
-      `psql #{database_url} -Atc "SELECT * FROM schema_migrations;"`.split
+    def remote_migrations(database_url)
+      database_uri = URI.parse(database_url)
+      begin
+        ENV['PGPASSWORD'] = database_uri.password
+        ENV['PGSSLMODE']  = 'require'
+        psql_cmd = "psql -U #{database_uri.user} -h #{database_uri.host} -p #{database_uri.port || 5432} #{database_uri.path[1..-1]}"
+        psql_output = `#{psql_cmd} -Atc "SELECT * FROM schema_migrations;" 2>&1`
+        if psql_output =~ /ERROR:  relation "schema_migrations" does not exist/
+          puts "'schema_migrations' table doesn't exist remotely, assuming no remote migrations"
+          return []
+        end
+        raise 'Error getting remote migrations' unless $?.success?
+        psql_output.split.map(&:to_i)
+      ensure
+        ENV.delete('PGPASSWORD')
+        ENV.delete('PGSSLMODE')
+      end
+    end
+
+    # returns an array of schema migration versions in the local source directory
+    def local_migrations
+      Rake::Task[:environment].invoke
+      ActiveRecord::Base.establish_connection
+      ActiveRecord::Migrator.migrations(ActiveRecord::Migrator.migrations_paths).map(&:version)
     end
 
   end
