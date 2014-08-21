c02f44f60aa8aba2259a9715257b9b6d4ddfcfbe
Joel Turkel <jturkel@salsify.com>
2014-08-21 14:52:29 -0400

    Fix #168 - Detect errors running git commands.

--- lib/heroku_san/git.rb
+++ lib/heroku_san/git.rb
@@ -1,5 +1,6 @@
 require 'rake'
 require 'rake/dsl_definition'
+require 'open3'
 
 module HerokuSan
   module Git
@@ -8,21 +9,21 @@ module HerokuSan
     include Rake::DSL
 
     def git_clone(repos, dir)
-      sh "git clone #{repos} #{dir}"
+      run_subprocess("git clone #{repos} #{dir}")
     end
 
     def git_active_branch
-      %x{git branch}.split("\n").select { |b| b =~ /^\*/ }.first.split(" ").last.strip
+      run_subprocess("git branch").split("\n").select { |b| b =~ /^\*/ }.first.split(" ").last.strip
     end
 
     def git_push(commit, repo, options = [])
       commit ||= "HEAD"
       options ||= []
       begin
-        sh "git update-ref refs/heroku_san/deploy #{commit}^{commit}"
-        sh "git push #{repo} #{options.join(' ')} refs/heroku_san/deploy:refs/heads/master"
+        run_subprocess("git update-ref refs/heroku_san/deploy #{commit}^{commit}")
+        run_subprocess("git push #{repo} #{options.join(' ')} refs/heroku_san/deploy:refs/heads/master")
       ensure
-        sh "git update-ref -d refs/heroku_san/deploy"
+        run_subprocess("git update-ref -d refs/heroku_san/deploy")
       end
     end
 
@@ -32,20 +33,31 @@ module HerokuSan
 
     def git_rev_parse(ref)
       return nil if ref.nil?
-      %x{git rev-parse #{ref}}.split("\n").first
+      run_subprocess("git rev-parse #{ref}").split("\n").first
     end
 
     def git_tag(glob)
       return nil if glob.nil?
-      %x{git tag -l '#{glob}'}.split("\n").last || (raise NoTagFoundError, "No tag found [#{glob}]")
+      run_subprocess("git tag -l '#{glob}'").split("\n").last || (raise NoTagFoundError, "No tag found [#{glob}]")
     end
 
     def git_revision(repo)
-      %x{git ls-remote --heads #{repo} master}.split.first
+      run_subprocess("git ls-remote --heads #{repo} master").split.first
     end
 
     def git_named_rev(ref)
-      %x{git name-rev #{ref}}.chomp
+      run_subprocess("git name-rev #{ref}").chomp
+    end
+
+    private
+
+    def run_subprocess(command)
+      output, error, status = Open3.capture3(command)
+      if status.success?
+        output
+      else
+        raise "Command '#{command}' failed with status #{status.exitstatus}: #{error}"
+      end
     end
   end
 end
--- spec/heroku_san/git_spec.rb
+++ spec/heroku_san/git_spec.rb
@@ -4,83 +4,121 @@ require 'heroku_san/git'
 class GitTest; include HerokuSan::Git; end
 
 describe GitTest do
-  describe "#git_push" do    
+  describe "#git_push" do
     it "pushes to heroku" do
-      subject.should_receive(:sh).with("git update-ref refs/heroku_san/deploy HEAD^{commit}")
-      subject.should_receive(:sh).with("git push git@heroku.com:awesomeapp.git  refs/heroku_san/deploy:refs/heads/master")
-      subject.should_receive(:sh).with("git update-ref -d refs/heroku_san/deploy")
+      expect_successful_subprocess("git update-ref refs/heroku_san/deploy HEAD^{commit}")
+      expect_successful_subprocess("git push git@heroku.com:awesomeapp.git  refs/heroku_san/deploy:refs/heads/master")
+      expect_successful_subprocess("git update-ref -d refs/heroku_san/deploy")
       subject.git_push(nil, 'git@heroku.com:awesomeapp.git')
     end
-    
+
     it "pushes a specific commit to heroku" do
-      subject.should_receive(:sh).with("git update-ref refs/heroku_san/deploy kommit^{commit}")
-      subject.should_receive(:sh).with("git push git@heroku.com:awesomeapp.git  refs/heroku_san/deploy:refs/heads/master")
-      subject.should_receive(:sh).with("git update-ref -d refs/heroku_san/deploy")
+      expect_successful_subprocess("git update-ref refs/heroku_san/deploy kommit^{commit}")
+      expect_successful_subprocess("git push git@heroku.com:awesomeapp.git  refs/heroku_san/deploy:refs/heads/master")
+      expect_successful_subprocess("git update-ref -d refs/heroku_san/deploy")
       subject.git_push('kommit', 'git@heroku.com:awesomeapp.git')
     end
-    
+
     it "includes options, too" do
-      subject.should_receive(:sh).with("git update-ref refs/heroku_san/deploy HEAD^{commit}")
-      subject.should_receive(:sh).with("git push git@heroku.com:awesomeapp.git --force -v refs/heroku_san/deploy:refs/heads/master")
-      subject.should_receive(:sh).with("git update-ref -d refs/heroku_san/deploy")
+      expect_successful_subprocess("git update-ref refs/heroku_san/deploy HEAD^{commit}")
+      expect_successful_subprocess("git push git@heroku.com:awesomeapp.git --force -v refs/heroku_san/deploy:refs/heads/master")
+      expect_successful_subprocess("git update-ref -d refs/heroku_san/deploy")
       subject.git_push(nil, 'git@heroku.com:awesomeapp.git', %w[--force -v])
     end
 
     it "propagates any errors, but still cleans up" do
-      subject.should_receive(:sh).with("git update-ref refs/heroku_san/deploy HEAD^{commit}")
-      subject.should_receive(:sh).with("git push git@heroku.com:awesomeapp.git  refs/heroku_san/deploy:refs/heads/master").and_raise
-      subject.should_receive(:sh).with("git update-ref -d refs/heroku_san/deploy")
+      expect_successful_subprocess("git update-ref refs/heroku_san/deploy HEAD^{commit}")
+      expect_failed_subprocess("git push git@heroku.com:awesomeapp.git  refs/heroku_san/deploy:refs/heads/master")
+      expect_successful_subprocess("git update-ref -d refs/heroku_san/deploy")
       expect { subject.git_push(nil, 'git@heroku.com:awesomeapp.git') }.to raise_error
     end
   end
 
   describe "#git_tag" do
     it "returns the latest tag that matches the pattern" do
-      subject.should_receive("`").with("git tag -l 'pattern*'") { "x\n\y\n\z\n" }
+      expect_successful_subprocess("git tag -l 'pattern*'", "x\n\y\n\z\n")
       subject.git_tag('pattern*').should == "z"
     end
 
     it "raises exception if no tags match the pattern" do
-      subject.should_receive("`").with("git tag -l 'pattern*'") { "\n" }
+      expect_successful_subprocess("git tag -l 'pattern*'")
       expect {
         subject.git_tag('pattern*')
       }.to raise_error(HerokuSan::Git::NoTagFoundError)
     end
 
     it "returns nil for a nil glob" do
-      subject.should_not_receive("`").with("git tag -l ''") { "\n" }
+      Open3.should_not_receive(:capture3)
       subject.git_tag(nil).should == nil
     end
+
+    it "raises an exception if the git process fails" do
+      expect_failed_subprocess("git tag -l 'pattern*'")
+      expect {
+        subject.git_tag('pattern*')
+      }.to raise_error(RuntimeError)
+    end
   end
-  
+
   describe "#git_rev_parse" do
     it "returns the rev based on the tag" do
-      subject.should_receive("`").with("git rev-parse prod/1234567890") { "sha\n" }
+      expect_successful_subprocess("git rev-parse prod/1234567890", "sha\n")
       subject.git_rev_parse('prod/1234567890').should == "sha"
     end
 
     it "returns nil for a blank tag" do
-      subject.should_not_receive("`").with("git rev-parse ") { "\n" }
+      Open3.should_not_receive(:capture3)
       subject.git_rev_parse(nil).should == nil
+    end
+
+    it "raises an exception if the git process fails" do
+      expect_failed_subprocess("git rev-parse prod/1234567890")
+      expect {
+        subject.git_rev_parse('prod/1234567890')
+      }.to raise_error(RuntimeError)
     end
   end
 
   describe "#git_revision" do
     it "returns the current revision of the repository (on Heroku)" do
-      subject.should_receive("`").with("git ls-remote --heads staging master") { "sha\n" }
+      expect_successful_subprocess("git ls-remote --heads staging master", "sha\n")
       subject.git_revision('staging').should == 'sha'
     end
-    
+
     it "returns nil if there is no revision (i.e. not deployed yet)" do
-      subject.should_receive("`").with("git ls-remote --heads staging master") { "\n" }
+      expect_successful_subprocess("git ls-remote --heads staging master")
       subject.git_revision('staging').should == nil
     end
+
+    it "raises an exception if the git process fails" do
+      expect_failed_subprocess("git ls-remote --heads staging master")
+      expect {
+        subject.git_revision('staging')
+      }.to raise_error(RuntimeError)
+    end
   end
-  
+
   describe "#git_named_rev" do
     it "returns symbolic names for given rev" do
-      subject.should_receive("`").with("git name-rev sha") {"sha production/123456\n"}
+      expect_successful_subprocess("git name-rev sha", "sha production/123456\n")
       subject.git_named_rev('sha').should == 'sha production/123456'
     end
+
+    it "raises an exception if the git process fails" do
+      expect_failed_subprocess("git name-rev sha", "sha production/123456\n")
+      expect {
+        subject.git_named_rev('sha')
+      }.to raise_error(RuntimeError)
+    end
+  end
+
+  def expect_successful_subprocess(command, stdout = "\n")
+    process_status = double(success?: true)
+    Open3.should_receive(:capture3).with(command) { [stdout, '', process_status] }
+  end
+
+  def expect_failed_subprocess(command, stderr = "\n")
+    process_status = double(success?: false, exitstatus: 123)
+    Open3.should_receive(:capture3).with(command) { ['', stderr, process_status] }
   end
 end
